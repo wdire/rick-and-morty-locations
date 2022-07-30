dc310f67f09bf93943cf5612a54c7b433765f752
Ramazan <wdire@t-x.local>
2022-07-30 20:23:22 +0300

    Added favicons

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,23 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icon/favicon-16x16.png"
+        />
       </Head>
       <body>
         <Main />
